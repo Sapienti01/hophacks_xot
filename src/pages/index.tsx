54e11432a023dc3989bc7184658e91afc8c0313a
Tarik <yildirimtarikcan@gmail.com>
2023-09-16 16:00:53 -0400

    added the past appointments table, and header links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
 import { Center } from "@mantine/core";
-import Head from "next/head";
-import Link from "next/link";
 import Recorder from "~/components/Recorder";
 
-import { api } from "~/utils/api";
-
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-
       <Center style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Recorder />
+          <Recorder />
       </Center>
-
+      
   );
 }
