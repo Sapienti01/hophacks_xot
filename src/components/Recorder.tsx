2411a16e72f79a801c0d6521c733b77f255bc7c0
Xinan Rahman <xinanrahman03@gmail.com>
2023-09-17 01:25:37 -0400

    feat: openai formatting + simplify

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,5 +1,14 @@
 /* eslint-disable */
-import { Button, Center, Container, LoadingOverlay, Stack, Title } from "@mantine/core";
+import {
+  Button,
+  Center,
+  Container,
+  LoadingOverlay,
+  Modal,
+  Paper,
+  Stack,
+  Title,
+} from "@mantine/core";
 import axios from "axios";
 import { useState } from "react";
 import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
@@ -8,6 +17,8 @@ import { createId } from "@paralleldrive/cuid2";
 import { Routes } from "~/utils/types";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/router";
+import { set } from "zod";
+import { marked } from "marked";
 
 const Recorder = () => {
   const router = useRouter();
@@ -15,15 +26,74 @@ const Recorder = () => {
   const [visible, setVisible] = useState<boolean>(false);
   const { mutateAsync: fetchPresignedUrls } =
     api.s3.getStandardUploadPresignedUrl.useMutation();
-  const { mutateAsync: fetchWhisper } = api.whisper.get.useMutation({onSuccess: (data) => {
-    router.push("transcript/" + data.id);
-  }});
+  const { mutateAsync: updateTranscription } =
+    api.transcription.updateTranscription.useMutation();
+  api.s3.getStandardUploadPresignedUrl.useMutation();
+  const { mutateAsync: fetchWhisper } = api.whisper.get.useMutation({
+    onSuccess: (whisperData) => {
+      handleWhisperData(whisperData);
+      console.log("successfully whispered");
+    },
+  });
   const [myKey, setMyKey] = useState<string>("");
   const [myBlob, setMyBlob] = useState<Blob | null>(null);
-  const {user} = useUser();
-
+  const { user } = useUser();
+  const [chunks, setChunks] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [modalOpen, setModalOpen] = useState(false);
 
   const recorderControls = useAudioRecorder();
+
+  async function handleWhisperData(whisperData: any) {
+    const response = await fetch(Routes.gpt, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: whisperData.data,
+      }),
+    });
+    if (!response) {
+      return;
+    }
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data: ReadableStream<BufferSource> | null = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+    let total = "";
+
+    setModalOpen(true);
+    setVisible(false);
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      if (!value) continue;
+      const chunkValue = decoder.decode(value);
+      setChunks((prev) => prev + chunkValue);
+      total += chunkValue;
+      setText(total);
+    }
+    setVisible(true);
+    setModalOpen(false);
+
+    await updateTranscription({
+      id: whisperData.id,
+      data: total,
+    });
+    setVisible(false);
+
+    router.push("/transcript/" + whisperData.id);
+  }
 
   if (user === undefined || user === null) {
     return <div>loading...</div>;
@@ -34,7 +104,7 @@ const Recorder = () => {
       await fetchWhisper({ key: key, userId: user.id });
     }
   }
-  
+
   const addAudioElement = async (blob: Blob) => {
     const url = URL.createObjectURL(blob);
     const audio = document.createElement("audio");
@@ -51,12 +121,10 @@ const Recorder = () => {
         setPresignedUrl(url);
       })
       .catch((err) => console.error(err));
-    
   };
 
-
   const onClick = async () => {
-    if(presignedUrl && myBlob) {
+    if (presignedUrl && myBlob) {
       setVisible(true);
       await axios
         .put(presignedUrl, myBlob.slice(), {
@@ -68,26 +136,33 @@ const Recorder = () => {
         })
         .catch((err) => console.error(err));
       await handleWhisperAudio(myKey);
-      setVisible(false);
-  }
-}
-
+    }
+  };
 
   return (
-    <Stack>
-      <LoadingOverlay visible={visible} overlayBlur={2} />
-      <Title>Record your Appointment</Title>
-      <Center>
-        <AudioRecorder
-          onRecordingComplete={(blob) => void addAudioElement(blob)}
-          recorderControls={recorderControls}
-          showVisualizer={true}
-        />
-        { presignedUrl && myBlob &&
-        <Button onClick = {onClick} variant="outline">Save</Button>
-}
-      </Center>
-    </Stack>
+    <>
+      <Modal opened={modalOpen} onClose={() => setModalOpen(true)}>
+        <Paper shadow="md" radius={"md"}>
+          <div dangerouslySetInnerHTML={{ __html: marked(chunks) }} />
+        </Paper>
+      </Modal>
+      <Stack>
+        <LoadingOverlay visible={visible} overlayBlur={2} />
+        <Title>Record your Appointment</Title>
+        <Center>
+          <AudioRecorder
+            onRecordingComplete={(blob) => void addAudioElement(blob)}
+            recorderControls={recorderControls}
+            showVisualizer={true}
+          />
+          {presignedUrl && myBlob && (
+            <Button onClick={onClick} variant="outline">
+              Save
+            </Button>
+          )}
+        </Center>
+      </Stack>
+    </>
   );
 };
 
